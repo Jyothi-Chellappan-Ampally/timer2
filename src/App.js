0fef3190bdc2212fd02f1832b3bd433719f29316
Jyothi Ampally <jyothiampally@gmail.com>
2023-10-16 15:22:31 +0530

    commited by jyothiampally
    Signed-off-by: Jyothi Ampally <jyothiampally@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import { ReactDOM } from 'react';
+//import { useState, useEffect } from 'react';
+import Timer from './components/Timer2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+ 
+// var interval=0;
+// const Timer = () => {
+//   const [days, setDays] = useState(0);
+//   const [hours, setHours] = useState(0);
+//   const [minutes, setMinutes] = useState(0);
+//   const [seconds, setSeconds] = useState(0);
+
+//   const deadline = "December, 31, 2023";
+
+//   const getTime = () => {
+//     const time = Date.parse(deadline) - Date.now();
+// // eslint-disable-next-line
+//     setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+//     // eslint-disable-next-line
+//     setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+//     // eslint-disable-next-line
+//     setMinutes(Math.floor((time / 1000 / 60) % 60));
+//     // eslint-disable-next-line
+//     setSeconds(Math.floor((time / 1000) % 60));
+//   };
+// //console.log(days);console.log(hours);console.log(minutes);console.log(seconds);
+// //console.log(getTime());
+//   useEffect(() => {
+//      interval = setInterval(() => getTime(deadline), 1000);
+
+//     return () => clearInterval(interval);
+//   }, []);
+
+   const   App=()=>{
+   
+      return (
+        
+       <div className="timer">
+   
+          <Timer/>
+    
+     </div>
+  
+   )
+     }
+  
+   
 export default App;
---
+++ src/components/newyear.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import { ReactDOM } from 'react';
+//import { useState, useEffect } from 'react';
+//import Timer from './components/Timer2';
+
+const   ny=()=>{
+   
+    return (
+      
+     <div className="ny">
+ 
+ <img src="./ny2024.jpg" alt='newyearimage'   id='img1'></img>
+  
+   </div>
+
+ )
+   }
+   export default ny;
